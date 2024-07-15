9ef219c5e5ed2b9b348818042a8396118e1382b6
Kiraradi <katel1998@yandex.ru>
2024-07-15 15:52:21 +0300

    fix style in BurgerIngredients component

--- src/components/BurgerIngredients/BurgerIngredients.tsx
+++ src/components/BurgerIngredients/BurgerIngredients.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 import IBurgerIngredients, {IBurgerIngredient} from '../BurgerConstructor/IBurgerConstructor.types'
-import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 
 import s from './burgerIngredients.module.css'
 
 const BurgerIngredients:React.FC<IBurgerIngredients> = ({burgerIngredients}) => {
   const [lockedIngredients, setLockedIngredients] = useState<IBurgerIngredient[]>([]);
   const [openedIngredient, setOpenedIngredient] = useState<IBurgerIngredient[]>([]);
+  const [fullPrice, setFullPrice] = useState<number>(0);
 
   
-  
+  useEffect(() => {
+    const price = burgerIngredients.reduce((acc, ing) => acc + ing.price, 0);
+    setFullPrice(price)
+  }, [burgerIngredients])
+
   useEffect(() => {
     const lockedIng: IBurgerIngredient[] = [];
     const openedIng: IBurgerIngredient[] = []; 
@@ -20,51 +25,59 @@ const BurgerIngredients:React.FC<IBurgerIngredients> = ({burgerIngredients}) => 
     })
   }, [burgerIngredients])
 
-  console.log(lockedIngredients)
   return (
     <div className={s.wrapper}>
-      {
-        lockedIngredients.length && (
-          <ConstructorElement
-            type={'top'}
-            isLocked= {false}
-            text={lockedIngredients[0].name}
-            price={lockedIngredients[0].price}
-            thumbnail={lockedIngredients[0].image}
-          />
-        )
-      }
-      
-      <div className={s.list}>
+      <div className={s.fullList}>
         {
-          openedIngredient.map(ing => {
-            return (
-              <div className= {s.item}>
-              <DragIcon type="primary" />              
-              <ConstructorElement
-                      type={undefined}
-                      isLocked= {false}
-                      text={ing.name}
-                      price={ing.price}
-                      thumbnail={ing.image}
-                    />
-              </div>
+          lockedIngredients.length && (
+            <ConstructorElement
+              type={'top'}
+              isLocked= {false}
+              text={lockedIngredients[0].name}
+              price={lockedIngredients[0].price}
+              thumbnail={lockedIngredients[0].image}
+            />
+          )
+        }
+        
+        <div className={s.list}>
+          {
+            openedIngredient.map(ing => {
+              return (
+                <div className= {s.item}>
+                <DragIcon type="primary" />              
+                <ConstructorElement
+                        type={undefined}
+                        isLocked= {false}
+                        text={ing.name}
+                        price={ing.price}
+                        thumbnail={ing.image}
+                      />
+                </div>
 
-            )
-          })
+              )
+            })
+          }
+        </div>
+        {
+          lockedIngredients[1] && (
+            <ConstructorElement
+              type={'bottom'}
+              isLocked= {false}
+              text={lockedIngredients[1].name}
+              price={lockedIngredients[1].price}
+              thumbnail={lockedIngredients[1].image}
+            />
+          )
         }
       </div>
-      {
-        lockedIngredients[1] && (
-          <ConstructorElement
-            type={'bottom'}
-            isLocked= {false}
-            text={lockedIngredients[1].name}
-            price={lockedIngredients[1].price}
-            thumbnail={lockedIngredients[1].image}
-          />
-        )
-      }
+      <div className={s.price}>
+        <p className='text text_type_main-large'>{fullPrice}</p>
+        <CurrencyIcon type="primary" />
+        <Button htmlType="button" type="primary" size="medium">
+          Оформить заказ
+        </Button>
+      </div>
     </div>
   )
 }
